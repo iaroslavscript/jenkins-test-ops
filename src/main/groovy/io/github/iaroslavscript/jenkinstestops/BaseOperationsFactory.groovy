d7329568aa5a67558b59bc6cfa1be775b9ee8ce6
iaroslavscript <iaroslavscript@users.noreply.github.com>
2026-01-20 19:16:07 +0100

    [GH-13] Work in progress Tue 20 Jan 19:16:07 CET 2026 (closes GH-13)

--- src/main/groovy/io/github/iaroslavscript/jenkinstestops/BaseOperationsFactory.groovy
+++ src/main/groovy/io/github/iaroslavscript/jenkinstestops/BaseOperationsFactory.groovy
@@ -7,5 +7,16 @@ package io.github.iaroslavscript.jenkinstestops
  */
 abstract class BaseOperationsFactory implements OperationsFactory {
 
+    protected final Map<class, Closure> executorFactories = [:]
+    protected final Map<class, Closure> cachedExecutors = [:]
+
+    /** 
+     * Generic method to register a custom executor factory.
+     * @param interfaceType The class type of the executor interface.
+     * @param factory Closure that creates the executor instance.
+     */
+    def <T> void register(Class<T> interfaceType, Closure factory) {
+        executorFactories[interfaceType] = factory
+    }
 }
 
