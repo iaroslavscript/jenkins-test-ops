723242f5d279d7af69fdab3bad69481356fd9fdc
iaroslavscript <iaroslavscript@users.noreply.github.com>
2026-01-20 20:03:08 +0100

    [GH-13] Work in progress Tue 20 Jan 20:03:08 CET 2026 (closes GH-13)

--- src/main/groovy/io/github/iaroslavscript/jenkinstestops/OperationsFactory.groovy
+++ src/main/groovy/io/github/iaroslavscript/jenkinstestops/OperationsFactory.groovy
@@ -4,4 +4,12 @@ package io.github.iaroslavscript.jenkinstestops
  * Factory interface for creating operation objects that abstract external dependencies.
  * This enables dependency injection and makes testing easier by allowing test doubles.
  */
-interface OperationsFactory {}
+interface OperationsFactory {
+    
+    /**
+     * Generic method to register a custom executor factory.
+     * @param interfaceType The class type of the executor interface.
+     * @param factory Closure that creates the executor instance.
+     */
+    def <T> void register(Class<T> interfaceType, Closure factory)
+}
